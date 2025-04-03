d3249f5ff5d5e6583e8aacff78431e40a3f9fd60
matheusvb3 <98937378+matheusvb3@users.noreply.github.com>
2025-04-02 21:42:21 -0300

    Modifica lógica de atribuição de wbe para acomodar SB e SH

--- T1-P2/MIPS_monocycle.vhd
+++ T1-P2/MIPS_monocycle.vhd
@@ -285,10 +285,14 @@ begin
     -- Data to data memory comes from the second read register at register file
     data_out <= STD_LOGIC_VECTOR(readData2);
     
-    wbe <= "1111" when decodedInstruction = SW else 
-           "0011" when decodedInstruction = SH else   
-           "0001" when decodedInstruction = SB else   
-           "0000";                                                           
+    wbe <= "0001" when decodedInstruction = SB and result(1 downto 0) = "00" else
+           "0010" when decodedInstruction = SB and result(1 downto 0) = "01" else
+           "0100" when decodedInstruction = SB and result(1 downto 0) = "10" else
+           "1000" when decodedInstruction = SB and result(1 downto 0) = "11" else
+           "0011" when decodedInstruction = SH and result(1 downto 0) = "00" else
+           "1100" when decodedInstruction = SH and result(1 downto 0) = "10" else
+           "1111" when decodedInstruction = SW else
+           "0000";                                              
     
     ce <= '1' when LoadInstruction(decodedInstruction) or StoreInstruction(decodedInstruction) else '0';
     
