d92254c1f955675c5e5487f9b0393a8418dc0b96
matheusvb3 <98937378+matheusvb3@users.noreply.github.com>
2025-04-03 20:43:00 -0300

    Altera lógica do data_out (#32)
    * Update MIPS_monocycle_tb.vhd
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete T1-P1/bubble_wave.do
    
    * Add files via upload
    
    * Update t1_code.txt
    
    * Adiciona lógica do SLLV, SRLV e SRAV
    
    * Adiciona PDF de especificação do MIPS
    
    * Adiciona data_out ao arquivo .do
    
    * Altera lógica de atribuição de data_out

--- T1-P2/MIPS_monocycle.vhd
+++ T1-P2/MIPS_monocycle.vhd
@@ -271,7 +271,8 @@ begin
         x"000000" & std_logic_vector(readData2(7 downto 0))       when (decodedInstruction = SB and result(1 downto 0) = "11") else
         std_logic_vector(readData2(15 downto 0)) & x"0000"        when (decodedInstruction = SH and result(1 downto 0) = "00") else
         x"0000" & std_logic_vector(readData2(15 downto 0))        when (decodedInstruction = SH and result(1 downto 0) = "10") else
-        std_logic_vector(readData2); -- Para SW - Data to data memory comes from the second read register at register file
+        std_logic_vector(readData2)                               when  decodedInstruction = SW;
+        -- Data to data memory comes from the second read register at register file
 
     -- ALU output address the data memory
     dataAddress <= STD_LOGIC_VECTOR(result);
